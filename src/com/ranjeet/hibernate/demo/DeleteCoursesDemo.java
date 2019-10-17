88e46c2fbb7a4833179c379a3db3fd82bd084fcc
imranjeetsingh <ranjee970@gmail.com>
2019-10-17 21:43:02 +0530

    deleted course

---
+++ src/com/ranjeet/hibernate/demo/DeleteCoursesDemo.java
@@ -0,0 +1,48 @@
+package com.ranjeet.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ranjeet.hibernate.entity.Course;
+import com.ranjeet.hibernate.entity.Instructor;
+import com.ranjeet.hibernate.entity.InstructorDetail;
+import com.ranjeet.hibernate.entity.Review;
+import com.ranjeet.hibernate.entity.Student;
+
+public class DeleteCoursesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			System.out.println("Deleting course object ...");
+			
+			session.beginTransaction();
+			
+			int id = 10;
+			Course course = session.get(Course.class, id);
+			
+			session.delete(course);
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!!!!!!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
